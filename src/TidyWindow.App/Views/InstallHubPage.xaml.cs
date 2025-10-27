f85c925022dd1895b98ec8e0319c0c9d0b52e800
Dhanush S <dhanush13244@gmail.com>
2025-10-27 22:33:21 +0530

    install hub page rework

--- src/TidyWindow.App/Views/InstallHubPage.xaml.cs
+++ src/TidyWindow.App/Views/InstallHubPage.xaml.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using TidyWindow.App.ViewModels;
 
 namespace TidyWindow.App.Views;
@@ -11,6 +13,9 @@ public partial class InstallHubPage : Page
     private readonly InstallHubViewModel _viewModel;
     private bool _disposed;
     private bool _scrollHandlersAttached;
+    private bool _responsiveLayoutApplied;
+    private bool _isCompactLayout;
+    private bool _sizeHandlerAttached;
 
     public InstallHubPage(InstallHubViewModel viewModel)
     {
@@ -23,15 +28,21 @@ public partial class InstallHubPage : Page
 
     private void OnPageLoaded(object sender, RoutedEventArgs e)
     {
-        if (_scrollHandlersAttached)
+        if (!_scrollHandlersAttached)
         {
-            return;
+            AttachScrollHandler(BundlesList);
+            AttachScrollHandler(PackagesList);
+            AttachScrollHandler(OperationsList);
+            _scrollHandlersAttached = true;
         }
 
-        AttachScrollHandler(BundlesList);
-        AttachScrollHandler(PackagesList);
-        AttachScrollHandler(OperationsList);
-        _scrollHandlersAttached = true;
+        if (!_sizeHandlerAttached)
+        {
+            SizeChanged += OnPageSizeChanged;
+            _sizeHandlerAttached = true;
+        }
+
+        ApplyResponsiveLayout(ActualWidth);
     }
 
     private void OnPageUnloaded(object sender, RoutedEventArgs e)
@@ -44,8 +55,18 @@ public partial class InstallHubPage : Page
         Loaded -= OnPageLoaded;
         Unloaded -= OnPageUnloaded;
         DetachScrollHandlers();
+        if (_sizeHandlerAttached)
+        {
+            SizeChanged -= OnPageSizeChanged;
+            _sizeHandlerAttached = false;
+        }
         _viewModel.Dispose();
         _disposed = true;
+    }
+
+    private void OnPageSizeChanged(object sender, SizeChangedEventArgs e)
+    {
+        ApplyResponsiveLayout(e.NewSize.Width);
     }
 
     private void AttachScrollHandler(ItemsControl? control)
@@ -130,5 +151,92 @@ public partial class InstallHubPage : Page
         }
 
         return null;
+    }
+
+    private void ApplyResponsiveLayout(double width)
+    {
+        if (LayoutGrid is null || LeftColumn is null || QueueCard is null)
+        {
+            return;
+        }
+
+        if (width <= 0 || double.IsNaN(width))
+        {
+            width = ActualWidth;
+        }
+
+        const double breakpoint = 1240;
+        var shouldBeCompact = width <= breakpoint;
+
+        if (_responsiveLayoutApplied && shouldBeCompact == _isCompactLayout)
+        {
+            return;
+        }
+
+        _isCompactLayout = shouldBeCompact;
+
+        var animate = _responsiveLayoutApplied;
+
+        if (shouldBeCompact)
+        {
+            LayoutGrid.ColumnDefinitions[0].Width = new GridLength(1, GridUnitType.Star);
+            LayoutGrid.ColumnDefinitions[1].Width = new GridLength(0);
+
+            Grid.SetRow(LeftColumn, 0);
+            Grid.SetColumn(LeftColumn, 0);
+            Grid.SetRowSpan(LeftColumn, 1);
+            Grid.SetColumnSpan(LeftColumn, 2);
+            AnimateMargin(LeftColumn, new Thickness(0), animate);
+
+            Grid.SetRow(QueueCard, 1);
+            Grid.SetColumn(QueueCard, 0);
+            Grid.SetRowSpan(QueueCard, 1);
+            Grid.SetColumnSpan(QueueCard, 2);
+            AnimateMargin(QueueCard, new Thickness(0, 24, 0, 0), animate);
+        }
+        else
+        {
+            LayoutGrid.ColumnDefinitions[0].Width = new GridLength(3, GridUnitType.Star);
+            LayoutGrid.ColumnDefinitions[1].Width = new GridLength(2, GridUnitType.Star);
+
+            Grid.SetRow(LeftColumn, 0);
+            Grid.SetColumn(LeftColumn, 0);
+            Grid.SetRowSpan(LeftColumn, 2);
+            Grid.SetColumnSpan(LeftColumn, 1);
+            AnimateMargin(LeftColumn, new Thickness(0, 0, 24, 0), animate);
+
+            Grid.SetRow(QueueCard, 0);
+            Grid.SetColumn(QueueCard, 1);
+            Grid.SetRowSpan(QueueCard, 2);
+            Grid.SetColumnSpan(QueueCard, 1);
+            AnimateMargin(QueueCard, new Thickness(16, 0, 0, 0), animate);
+        }
+
+        AnimateMargin(RootScrollViewer, shouldBeCompact ? new Thickness(20) : new Thickness(32), animate);
+
+        _responsiveLayoutApplied = true;
+    }
+
+    private static void AnimateMargin(FrameworkElement element, Thickness target, bool animate)
+    {
+        if (element is null)
+        {
+            return;
+        }
+
+        if (!animate)
+        {
+            element.BeginAnimation(FrameworkElement.MarginProperty, null);
+            element.Margin = target;
+            return;
+        }
+
+        var animation = new ThicknessAnimation
+        {
+            To = target,
+            Duration = TimeSpan.FromMilliseconds(220),
+            EasingFunction = new CubicEase { EasingMode = EasingMode.EaseOut }
+        };
+        element.BeginAnimation(FrameworkElement.MarginProperty, animation);
     }
 }
